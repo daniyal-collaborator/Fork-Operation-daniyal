85553b7bb72f6f2597df95c1b3178124617ca7e5
daniyal <daniyalrouhbakhsh@yahoo.com>
2024-10-25 21:01:39 +0330

    Register Feature Complete!

--- JS/app.js
+++ JS/app.js
@@ -6,3 +6,12 @@ const isLogin = user => {
         return false
     }
 }
+
+const registerFeature = user => {
+    if(user == 'Daniyal'){
+        return true
+    }
+    else{
+        return false
+    }
+}
